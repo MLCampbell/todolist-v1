a5b00536ab99ded4c395f5becf4f74df00c5da2e
MLCampbell <marcus.l.campbell@gmail.com>
2021-01-12 21:15:14 +1300

    add layout and refactor

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const getDate = require('./date');
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
+const items = [];
+const workItems = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({exteneded: true}));
+app.use(express.static("public"));
+
 app.get('/', function (req, res) {
 
-    var today = new Date();
+    const day = date.getDate();
 
-    if (today.getDay() === 6 || today.getDay() === 0) {
-        res.send("It's the weekend!")
-    }
-    else {
-        res.send("Time to work.")
-    };
+    res.render("list", { listTitle: day, newListItems: items });
 
 });
 
+app.post('/', function (req, res) {
+
+    if (req.body.list === "Work") {
+        workItems.push(req.body.newItem);
+        res.redirect("/work")
+    } else {
+        items.push(req.body.newItem);
+        res.redirect("/");   
+    }  
+});
+
+app.get("/work", function(req,res) {
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function (req,res) {
+    res.render("about");
+});
 
 app.listen(3000, function () {
     console.log("Server started on 3k . . . lessago!")
-})
+});
